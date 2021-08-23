7b4ba7df65561424a2ac528ff1f65052f850753c
Manan Jethwani <2019282@iiitdmj.ac.in>
2021-08-23 21:48:27 +0530

    updated course schema and working

--- backend/models/course.js
+++ backend/models/course.js
@@ -2,50 +2,35 @@ const Joi = require("joi");
 const mongoose = require("mongoose");
 
 const courseSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 2,
-        maxlength: 50,
-    },
-    semester: {
-        type: Number,
-        required: true,
-    },
-    year: {
-        type: String,
-        required: true,
-    },
-    prof: {
-        type: String,
-        required: true,
-        minlength: 2,
-        maxlength: 50,
-    },
-    id: {
-        type: String,
-        required: true,
-    },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+  },
+  semester: {
+    type: Number,
+    required: true,
+  },
+  prof: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+  },
 });
 
 const Course = mongoose.model("Course", courseSchema);
 
-function insertCourse(data) {
-    const id = data.name + `${data.semester}` + `${data.year}` + data.prof;
-    return new Course({'name': data.name, 'semester': data.semester, 'year': data.year, 'prof': data.prof, 'id': id});
-}
-
 function validateCourse(course) {
-    const schema = {
-      name: Joi.string().min(2).max(50).required(),
-      semester: Joi.number().required(),
-      year: Joi.number().required(),
-      prof: Joi.string().min(2).max(50).required(),
-    };
-  
-    return Joi.validate(course, schema);
+  const schema = {
+    name: Joi.string().min(2).max(50).required(),
+    semester: Joi.number().required(),
+    prof: Joi.string().min(2).max(50).required(),
+  };
+
+  return Joi.validate(course, schema);
 }
 
 exports.Course = Course;
 exports.validate = validateCourse;
-exports.insertCourse = insertCourse;
--- backend/routes/course.js
+++ backend/routes/course.js
@@ -1,4 +1,4 @@
-const { validate, insertCourse, Course } = require('../models/course');
+const { validate, Course } = require("../models/course");
 const { Router } = require("express");
 const router = Router();
 // import admin from "../middleware/admin";
@@ -6,20 +6,33 @@ const _ = require("lodash");
 
 // TODO: use admin middleware to restrict in-valid entries.
 router.post("/", async (req, res) => {
-    const { error } = validate(req.body.data);
-    if (error) {
-        res.status(400).send(error.details[0].message);
-    }
+  const { error } = validate(req.body.data);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
 
-    const course = insertCourse(req.body.data);
-    await course.save();
+  let course = await Course.find({
+    name: req.body.name,
+    semester: req.body.semester,
+    prof: req.body.prof,
+  });
 
-    res.send((await Course.find({semester: req.body.semester})));
-    
+  if (course) {
+    return res.status(400).send("Course already exists.");
+  }
+
+  course = new Course({
+    name: req.body.name,
+    semester: req.body.semester,
+    prof: req.body.prof,
+  });
+  await course.save();
+
+  res.send(await Course.find({ semester: req.body.semester }));
 });
 
 router.get("/", async (req, res) => {
-    res.send((await Course.find({semester: req.body.semester})));
-})
+  res.send(await Course.find({ semester: req.body.semester }));
+});
 
 module.exports = router;
